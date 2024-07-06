f9a33c7fdf0f7a8d20b40c3948adab94dd20158d
SilvVF <david.silver232@gmail.com>
2024-07-06 17:57:06 -0400

    update session on disconnect

--- app/src/main/java/io/silv/oflchat/core/connection/ConnectionLifeCycleHandler.kt
+++ app/src/main/java/io/silv/oflchat/core/connection/ConnectionLifeCycleHandler.kt
@@ -4,15 +4,20 @@ import com.google.android.gms.nearby.connection.ConnectionInfo
 import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback
 import com.google.android.gms.nearby.connection.ConnectionResolution
 import com.google.android.gms.nearby.connection.ConnectionsStatusCodes
+import io.silv.oflchat.core.call.SignalingCommand
+import io.silv.oflchat.core.call.WebRTCSessionState
 import io.silv.oflchat.core.database.ConnectionDao
 import io.silv.oflchat.core.model.ConnectionEntity
 import io.silv.oflchat.core.model.ConnectionEntity.State
 import io.silv.oflchat.core.model.toUpdate
 import io.silv.oflchat.helpers.PayloadHelper
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
+import java.util.concurrent.ConcurrentHashMap
 
 
 class ConnectionLifeCycleHandler(
@@ -21,7 +26,13 @@ class ConnectionLifeCycleHandler(
     private val userInitiated: Boolean = false,
 ) : ConnectionLifecycleCallback() {
 
+    private val onLostTasks = ConcurrentHashMap<String, Job>()
+
     override fun onConnectionInitiated(eid: String, info: ConnectionInfo) {
+        onLostTasks[eid]?.cancel()
+        PayloadHelper.signalingClients[eid]?.handleCommand(
+            "${SignalingCommand.STATE} ${WebRTCSessionState.Ready}"
+        )
         scope.launch {
             val existing = connectionDao.selectByUserId(info.uuid)
 
@@ -67,8 +78,18 @@ class ConnectionLifeCycleHandler(
     }
 
     override fun onDisconnected(eid: String) {
-        scope.launch {
+        onLostTasks[eid] = scope.launch {
+            delay(3000)
+
+            ensureActive()
+
             val connection = connectionDao.selectByEndpointId(eid) ?: return@launch
+
+            ensureActive()
+
+            PayloadHelper.signalingClients[eid]?.handleCommand(
+                "${SignalingCommand.STATE} ${WebRTCSessionState.Impossible}"
+            )
 
             connectionDao.update(
                 connection.copy(
